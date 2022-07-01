c767d02fe80d99f06cdd7587dbe138f47badca80
Evandro Lima <evandro.lima@gat.digital>
2022-07-01 17:05:29 +0000

    Using Link component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,10 @@
-import { MouseEvent } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-  const navigate = useNavigate();
-
-  function handleAnchorClick(event: MouseEvent<HTMLAnchorElement>) {
-    event.preventDefault();
-    const newRoute: string | null = event.currentTarget.getAttribute("href");
-
-    if (newRoute) {
-      navigate(newRoute);
-    }
-  }
-
   return <nav>
     <ul>
-      <li><a onClick={handleAnchorClick} href="/">Home</a></li>
-      <li><a onClick={handleAnchorClick} href="/contact">Contact</a></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
     </ul>
   </nav>
 }
