85b9603856702e7289aa2f40bb85e79ac316624f
Evandro Lima <evandro.lima@gat.digital>
2022-07-03 11:01:34 +0000

    Feature - add user tag

--- src/app/views/Home.view.tsx
+++ src/app/views/Home.view.tsx
@@ -1,12 +1,14 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
 import PostsList from "../features/PostsList"
-import UserMetrics from "../features/UserMetrics"
+import UserMetrics from "../features/UserPerformance"
+import UserTopTags from "../features/UserTopTags"
 import DefaultLayout from '../layouts/Default/Default.layout'
 
 export default function Home() {
   usePageTitle('Home')
 
   return <DefaultLayout>
+    <UserTopTags />
     <UserMetrics />
     <PostsList />
   </DefaultLayout>
