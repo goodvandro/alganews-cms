1928b99e643c70f8c5f5db633950195fefaea39b
Evandro Lima <evandro.lima@gat.digital>
2022-07-17 09:27:28 +0000

    show post preview modal

--- src/app/features/PostsList.tsx
+++ src/app/features/PostsList.tsx
@@ -7,8 +7,10 @@ import withBoundary from "../../core/hoc/withBoundary"
 import { Post } from "../../sdk/@types"
 import PostService from "../../sdk/services/Post.service"
 import Table from "../components/Table/Table"
-import Skeleton from 'react-loading-skeleton';
+import Skeleton from 'react-loading-skeleton'
 import Loading from "../components/Loading"
+import PostPreview from "./PostPreview"
+import modal from "../../core/utils/modal"
 
 function PostsList() {
   const [posts, setPosts] = useState<Post.Paginated>()
@@ -44,50 +46,73 @@ function PostsList() {
         Header: () => <div style={{ textAlign: 'left' }}>Título</div>,
         accessor: 'title',
         width: 320,
-        Cell: (props) => <div style={{ textAlign: 'left', display: 'flex', gap: 8, alignItems: 'center' }}>
-          <img
-            width={24}
-            height={24}
-            src={props.row.original.editor.avatarUrls.small}
-            alt={props.row.original.editor.name}
-            title={props.row.original.editor.name}
-          />
-          {props.value}
-        </div>,
+        Cell: (props) => (
+          <div
+            style={{
+              textAlign: 'left',
+              display: 'flex',
+              gap: 8,
+              alignItems: 'center'
+            }}>
+            <img
+              width={24}
+              height={24}
+              src={props.row.original.editor.avatarUrls.small}
+              alt={props.row.original.editor.name}
+              title={props.row.original.editor.name}
+            />
+            <a
+              href={`/posts/${props.row.original.id}`}
+              onClick={(e) => {
+                e.preventDefault()
+                modal({
+                  children: <PostPreview postId={props.row.original.id} />,
+                })
+              }}
+            >
+              {props.value}
+            </a>
+            {props.value}
+          </div>
+        ),
       },
       {
         Header: () => <div style={{ textAlign: 'right' }}>Criação</div>,
         accessor: 'createdAt',
-        Cell: (props) => <div
-          style={{
-            textAlign: 'right',
-            fontFamily: '"Roboto mono", monospace'
-          }}
-        >
-          {format(new Date(props.value), 'dd/MM/yyyy')}
-        </div>,
+        Cell: (props) => (
+          <div
+            style={{
+              textAlign: 'right',
+              fontFamily: '"Roboto mono", monospace'
+            }}
+          >
+            {format(new Date(props.value), 'dd/MM/yyyy')}
+          </div>
+        ),
       },
       {
         Header: () => <div style={{ textAlign: 'right' }}>Última atualização</div>,
         accessor: 'updatedAt',
-        Cell: (props) => <div
-          style={{
-            textAlign: 'right',
-            fontFamily: '"Roboto mono", monospace'
-          }}
-        >
-          {format(new Date(props.value), 'dd/MM/yyyy')}
-        </div>,
+        Cell: (props) => (
+          <div
+            style={{
+              textAlign: 'right',
+              fontFamily: '"Roboto mono", monospace'
+            }}
+          >
+            {format(new Date(props.value), 'dd/MM/yyyy')}
+          </div>
+        ),
       },
       {
         id: Math.random().toString(),
         accessor: 'published',
         Header: () => <div style={{ textAlign: 'right' }}>Ações</div>,
-        Cell: (props) => <div style={{ textAlign: 'right' }}>
-          {
-            props.value ? 'Publicado' : 'Privado'
-          }
-        </div>,
+        Cell: (props) => (
+          <div style={{ textAlign: 'right' }}>
+            {props.value ? 'Publicado' : 'Privado'}
+          </div>
+        ),
       },
     ],
     []
@@ -118,10 +143,7 @@ function PostsList() {
 
   return <>
     <Loading show={loading} />
-    <Table
-      instance={instance}
-      onPaginate={setPage}
-    />
+    <Table instance={instance} onPaginate={setPage} />
   </>
 }
 
