6d0f597ebfb3c797b5bb20dfbb8476aabdc78e78
Evandro Lima <evandro.lima@gat.digital>
2022-11-08 10:44:28 +0000

    Show user session detalhes

--- src/app/features/EditorProfile.tsx
+++ src/app/features/EditorProfile.tsx
@@ -1,8 +1,9 @@
-import { getEditorDescription } from "goodvandro-alganews-sdk";
+import { getEditorDescription, User } from "goodvandro-alganews-sdk";
 import { transparentize } from "polished";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useMemo } from "react";
+import { useParams } from "react-router";
 import styled from "styled-components";
+import useAuth from "../../core/hooks/useAuth";
 import useSingleEditor from "../../core/hooks/useSingleEditor";
 import FieldDescriptor from "../components/FieldDescriptor/FieldDescriptor";
 import ProgressBar from "../components/ProgressBar/ProgressBar";
@@ -13,98 +14,130 @@ interface EditorProfileProps {
 }
 
 function EditorProfile(props: EditorProfileProps) {
-  const params = useParams<{ id: string }>()
-  const { editor, fetchEditor } = useSingleEditor()
+  const params = useParams<{ id: string }>();
+
+  const { editor, fetchEditor } = useSingleEditor();
+  const { user } = useAuth();
+
+  const editorIsAuthenticatedUser = useMemo(
+    () => Number(params.id) === user?.id,
+    [user, params.id]
+  );
+
+  const editorData = useMemo(
+    () => (editorIsAuthenticatedUser ? user : editor),
+    [editorIsAuthenticatedUser, user, editor]
+  );
 
   useEffect(() => {
-    fetchEditor(Number(params.id))
-  }, [fetchEditor, params.id])
+    if (!editorIsAuthenticatedUser) fetchEditor(Number(params.id));
+  }, [fetchEditor, params.id, editorIsAuthenticatedUser]);
 
-  if (!editor) return null
+  return (
+    <EditorProfileWrapper>
+      <EditorHeadline>
+        <Avatar src={editorData?.avatarUrls.small} />
+        <Name>{editorData?.name}</Name>
+        {editorData?.createdAt && (
+          <Description>
+            {getEditorDescription(new Date(editorData.createdAt))}
+          </Description>
+        )}
+      </EditorHeadline>
 
-  return <EditorProfileWrapper>
-    <EditorHeadline>
-      <Avatar src={editor.avatarUrls.small} />
-      <Name>{editor.name}</Name>
-      <Description>
-        {getEditorDescription(new Date(editor.createdAt))}
-      </Description>
-    </EditorHeadline>
+      <Divisor />
 
-    <Divisor />
-
-    <EditorFeatures>
-      <PersonalInfo>
-        <Biography>{editor.bio}</Biography>
-        <Skills>
-          {
-            editor.skills?.map((skill) => {
-              return <ProgressBar
-                key={skill.name}
-                progress={skill.percentage}
-                title={skill.name}
-                theme={'primary'}
-              />
-            })
-          }
-        </Skills>
-      </PersonalInfo>
-      <ContactInfo>
-        <FieldDescriptor field={'Cidade'} value={editor.location.city} />
-        <FieldDescriptor field={'Estado'} value={editor.location.state} />
-        {
-          !props.hidePersonalData && <>
+      <EditorFeatures>
+        <PersonalInfo>
+          {editorData?.bio && <Biography>{editorData?.bio}</Biography>}
+          <Skills>
+            {editorData?.skills?.map((skill) => {
+              return (
+                <ProgressBar
+                  key={skill.name}
+                  progress={skill.percentage}
+                  title={skill.name}
+                  theme={"primary"}
+                />
+              );
+            })}
+          </Skills>
+        </PersonalInfo>
+        <ContactInfo>
+          {editorData?.location.city && (
             <FieldDescriptor
-              field={'Telefone'}
-              value={'+55 27 99900-9999'}
+              field={"Cidade"}
+              value={editorData?.location.city}
             />
+          )}
+          {editorData?.location.state && (
             <FieldDescriptor
-              field={'Email'}
-              value={'ana.castillo@redacao.algacontent.com'}
+              field={"Estado"}
+              value={editorData?.location.state}
             />
+          )}
+
+          {(editorData as User.Detailed)?.phone && (
             <FieldDescriptor
-              field={'Nascimento'}
-              value={'26 de Dezembro de 1997 (22 anos)'}
+              field={"Telefone"}
+              value={(editorData as User.Detailed)?.phone}
             />
-          </>
-        }
-      </ContactInfo>
-    </EditorFeatures>
-    {
-      !props.hidePersonalData && <EditorEarnings >
-        <ValueDescriptor
-          color={'default'}
-          value={21452}
-          description={'Palavras nesta semana'}
-        />
-        <ValueDescriptor
-          color={'default'}
-          value={123234}
-          description={'Palavras no mês'}
-        />
-        <ValueDescriptor
-          color={'default'}
-          value={12312312}
-          description={'Total de palavras'}
-        />
-        <ValueDescriptor
-          color={'primary'}
-          value={545623.23}
-          description={'Ganhos na semana'} isCurrency
-        />
-        <ValueDescriptor
-          color={'primary'}
-          value={545623.23}
-          description={'Ganhos no mês'} isCurrency
-        />
-        <ValueDescriptor
-          color={'primary'}
-          value={545623.23}
-          description={'Ganhos no total'} isCurrency
-        />
-      </EditorEarnings>
-    }
-  </EditorProfileWrapper>
+          )}
+          {(editorData as User.Detailed)?.email && (
+            <FieldDescriptor
+              field={"Email"}
+              value={(editorData as User.Detailed)?.email}
+            />
+          )}
+          {(editorData as User.Detailed)?.birthdate && (
+            <FieldDescriptor
+              field={"Nascimento"}
+              value={(editorData as User.Detailed)?.birthdate}
+            />
+          )}
+        </ContactInfo>
+      </EditorFeatures>
+      {(editorData as User.Detailed)?.metrics && (
+        <>
+          <EditorEarnings>
+            <ValueDescriptor
+              color={"default"}
+              value={(editorData as User.Detailed)?.metrics.weeklyWords}
+              description={"Palavras nesta semana"}
+            />
+            <ValueDescriptor
+              color={"default"}
+              value={(editorData as User.Detailed)?.metrics.monthlyWords}
+              description={"Palavras no mês"}
+            />
+            <ValueDescriptor
+              color={"default"}
+              value={(editorData as User.Detailed)?.metrics.lifetimeWords}
+              description={"Total de palavras"}
+            />
+            <ValueDescriptor
+              color={"primary"}
+              value={(editorData as User.Detailed)?.metrics.weeklyEarnings}
+              description={"Ganhos na semana"}
+              isCurrency
+            />
+            <ValueDescriptor
+              color={"primary"}
+              value={(editorData as User.Detailed)?.metrics.monthlyEarnings}
+              description={"Ganhos no mês"}
+              isCurrency
+            />
+            <ValueDescriptor
+              color={"primary"}
+              value={(editorData as User.Detailed)?.metrics.lifetimeEarnings}
+              description={"Ganhos no total"}
+              isCurrency
+            />
+          </EditorEarnings>
+        </>
+      )}
+    </EditorProfileWrapper>
+  );
 }
 
 const EditorProfileWrapper = styled.div`
@@ -112,8 +145,8 @@ const EditorProfileWrapper = styled.div`
   flex-direction: column;
   gap: 16px;
   padding: 24px;
-  border: 1px solid ${transparentize(0.9, '#274060')};
-`
+  border: 1px solid ${transparentize(0.9, "#274060")};
+`;
 
 const EditorHeadline = styled.div`
   display: grid;
@@ -122,7 +155,7 @@ const EditorHeadline = styled.div`
   grid-template-rows: 2;
   grid-template-columns: 48px auto;
   height: 48px;
-`
+`;
 
 const Avatar = styled.img`
   grid-row-start: 1;
@@ -130,59 +163,59 @@ const Avatar = styled.img`
   object-fit: contain;
   width: 48px;
   height: 48px;
-`
+`;
 
 const Name = styled.h1`
   font-size: 18px;
   font-weight: 400;
   grid-column-start: 2;
-`
+`;
 
 const Description = styled.span`
   font-size: 12px;
   grid-column-start: 2;
-`
+`;
 
 const Divisor = styled.div`
-  border-bottom: 1px solid  ${transparentize(0.9, '#274060')};
-`
+  border-bottom: 1px solid ${transparentize(0.9, "#274060")};
+`;
 
 const EditorFeatures = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 2;
   gap: 24px;
-`
+`;
 
 const PersonalInfo = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
-`
+`;
 
 const Biography = styled.p`
   font-size: 12px;
   line-height: 20px;
-`
+`;
 
 const ContactInfo = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 16px 0;
-  >* {
+  > * {
     width: 100%;
   }
-  &>:nth-child(1),
-  &>:nth-child(2) {
+  & > :nth-child(1),
+  & > :nth-child(2) {
     width: 50%;
   }
-`
+`;
 
 const Skills = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
-`
+`;
 
 const EditorEarnings = styled.div`
   display: grid;
@@ -190,6 +223,6 @@ const EditorEarnings = styled.div`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
   grid-gap: 24px;
-`
+`;
 
-export default EditorProfile
+export default EditorProfile;
--- src/app/views/EditorProfile.view.tsx
+++ src/app/views/EditorProfile.view.tsx
@@ -1,10 +1,10 @@
-import DefaultLayout from '../layouts/Default/Default.layout';
-import EditorProfile from '../features/EditorProfile';
+import DefaultLayout from "../layouts/Default/Default.layout";
+import EditorProfile from "../features/EditorProfile";
 
 export default function EditorProfileView() {
   return (
     <DefaultLayout>
-      <EditorProfile hidePersonalData />
+      <EditorProfile />
     </DefaultLayout>
-  )
+  );
 }
