17c9da80b2e6c8fab923b66acaef7b3659ca5739
Evandro Lima <evandro.lima@gat.digital>
2022-11-08 10:49:39 +0000

    Link post to blog

--- src/app/features/PostsList.tsx
+++ src/app/features/PostsList.tsx
@@ -1,21 +1,21 @@
-import { mdiOpenInNew } from "@mdi/js"
-import Icon from "@mdi/react"
-import { format } from "date-fns"
-import { Post } from 'goodvandro-alganews-sdk'
-import { useEffect, useMemo, useState } from "react"
-import Skeleton from 'react-loading-skeleton'
-import { Column, usePagination, useTable } from "react-table"
-import withBoundary from "../../core/hoc/withBoundary"
-import usePosts from "../../core/hooks/usePosts"
-import modal from "../../core/utils/modal"
-import Loading from "../components/Loading"
-import PostTitleAnchor from "../components/PostTitleAnchor"
-import Table from "../components/Table/Table"
-import PostPreview from "./PostPreview"
+import { mdiOpenInNew } from "@mdi/js";
+import Icon from "@mdi/react";
+import { format } from "date-fns";
+import { Post } from "goodvandro-alganews-sdk";
+import { useEffect, useMemo, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import { Column, usePagination, useTable } from "react-table";
+import withBoundary from "../../core/hoc/withBoundary";
+import usePosts from "../../core/hooks/usePosts";
+import modal from "../../core/utils/modal";
+import Loading from "../components/Loading";
+import PostTitleAnchor from "../components/PostTitleAnchor";
+import Table from "../components/Table/Table";
+import PostPreview from "./PostPreview";
 
 function PostsList() {
   const { loading, paginatedPosts, fetchPosts } = usePosts();
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     fetchPosts({
@@ -23,29 +23,40 @@ function PostsList() {
       size: 7,
       showAll: true,
       sort: ["createdAt", "desc"],
-    })
-  }, [fetchPosts, page])
+    });
+  }, [fetchPosts, page]);
 
   const columns = useMemo<Column<Post.Summary>[]>(
     () => [
       {
-        Header: '',
-        accessor: 'id', // accessor is the "key" in the data
-        Cell: () => <Icon path={mdiOpenInNew} size={'14px'} color={'#09f'} />
+        Header: "",
+        accessor: "id", // accessor is the "key" in the data
+        Cell: ({ row }) => (
+          <div style={{ paddingLeft: 8, width: "16px" }}>
+            <a
+              target={"_blank"}
+              href={`http://localhost:3002/posts/${row.original.id}/${row.original.slug}`}
+              rel="noreferrer noopener"
+            >
+              <Icon path={mdiOpenInNew} size={"14px"} color={"#09f"} />
+            </a>
+          </div>
+        ),
       },
       {
-        Header: () => <div style={{ textAlign: 'left' }}>Título</div>,
-        accessor: 'title',
+        Header: () => <div style={{ textAlign: "left" }}>Título</div>,
+        accessor: "title",
         width: 320,
         Cell: (props) => (
           <div
             style={{
-              textAlign: 'left',
-              display: 'flex',
+              textAlign: "left",
+              display: "flex",
               gap: 8,
-              alignItems: 'center',
+              alignItems: "center",
               maxWidth: 270,
-            }}>
+            }}
+          >
             <img
               width={24}
               height={24}
@@ -57,10 +68,10 @@ function PostsList() {
               title={props.value}
               href={`/posts/${props.row.original.id}`}
               onClick={(e) => {
-                e.preventDefault()
+                e.preventDefault();
                 modal({
                   children: <PostPreview postId={props.row.original.id} />,
-                })
+                });
               }}
             >
               {props.value}
@@ -69,40 +80,42 @@ function PostsList() {
         ),
       },
       {
-        Header: () => <div style={{ textAlign: 'right' }}>Criação</div>,
-        accessor: 'createdAt',
+        Header: () => <div style={{ textAlign: "right" }}>Criação</div>,
+        accessor: "createdAt",
         Cell: (props) => (
           <div
             style={{
-              textAlign: 'right',
-              fontFamily: '"Roboto mono", monospace'
+              textAlign: "right",
+              fontFamily: '"Roboto mono", monospace',
             }}
           >
-            {format(new Date(props.value), 'dd/MM/yyyy')}
+            {format(new Date(props.value), "dd/MM/yyyy")}
           </div>
         ),
       },
       {
-        Header: () => <div style={{ textAlign: 'right' }}>Última atualização</div>,
-        accessor: 'updatedAt',
+        Header: () => (
+          <div style={{ textAlign: "right" }}>Última atualização</div>
+        ),
+        accessor: "updatedAt",
         Cell: (props) => (
           <div
             style={{
-              textAlign: 'right',
-              fontFamily: '"Roboto mono", monospace'
+              textAlign: "right",
+              fontFamily: '"Roboto mono", monospace',
             }}
           >
-            {format(new Date(props.value), 'dd/MM/yyyy')}
+            {format(new Date(props.value), "dd/MM/yyyy")}
           </div>
         ),
       },
       {
         id: Math.random().toString(),
-        accessor: 'published',
-        Header: () => <div style={{ textAlign: 'right' }}>Ações</div>,
+        accessor: "published",
+        Header: () => <div style={{ textAlign: "right" }}>Ações</div>,
         Cell: (props) => (
-          <div style={{ textAlign: 'right' }}>
-            {props.value ? 'Publicado' : 'Privado'}
+          <div style={{ textAlign: "right" }}>
+            {props.value ? "Publicado" : "Privado"}
           </div>
         ),
       },
@@ -122,21 +135,25 @@ function PostsList() {
   );
 
   if (!paginatedPosts)
-    return <div>
-      <Skeleton height={32} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-    </div>
+    return (
+      <div>
+        <Skeleton height={32} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </div>
+    );
 
-  return <>
-    <Loading show={loading} />
-    <Table instance={instance} onPaginate={setPage} />
-  </>
+  return (
+    <>
+      <Loading show={loading} />
+      <Table instance={instance} onPaginate={setPage} />
+    </>
+  );
 }
 
-export default withBoundary(PostsList, 'lista de posts')
+export default withBoundary(PostsList, "lista de posts");
