1c0ad2159008b6f371b31e956c2dddfe58e29200
Evandro Monteiro Lima <evandro.lima@gat.digital>
2022-11-18 09:38:28 +0000

    Config .env

--- src/auth/Authorization.service.ts
+++ src/auth/Authorization.service.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 import qs from "qs";
 import pkceChallenge from "pkce-challenge";
 
+const AUTH_SERVER = process.env.REACT_APP_AUTH_SERVER_BASE_URL;
+
 const authServer = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: AUTH_SERVER,
 });
 
 authServer.interceptors.response.use(undefined, async (error) => {
@@ -26,7 +28,7 @@ export interface OAuthAuthorizationTokenResponse {
 export default class AuthService {
   public static imperativelySendToLogout() {
     window.localStorage.clear();
-    window.location.href = `http://localhost:8081/logout?redirect=http://localhost:3000`;
+    window.location.href = `${AUTH_SERVER}/logout?redirect=http://localhost:3000`;
   }
 
   public static async getNewToken(config: {
@@ -84,7 +86,7 @@ export default class AuthService {
       code_challenge_method: "S256",
     });
 
-    return `http://localhost:8081/oauth/authorize?${config}`;
+    return `${AUTH_SERVER}/oauth/authorize?${config}`;
   }
 
   public static async imperativelySendToLoginScreen() {
